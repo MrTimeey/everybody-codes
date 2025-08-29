32c1049dab630e7db653b2259dac8d477db61e24
Tim Kruse <coding@tim-kruse.com>
2025-08-29 13:24:51 +0200

    feat: add ts project template

---
+++ project_templates/ts/src/xService.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest'
+import {part1, part2, part3} from './xService'
+
+test('Test input part 1', () => {
+  const input = [
+    '',
+  ]
+  expect(part1(input)).toBe(0)
+})
+
+test('Test input part 2', () => {
+  const input = [
+    '',
+  ]
+  expect(part2(input)).toBe(0)
+})
+
+test('Test input part 3', () => {
+  const input = [
+    '',
+  ]
+  expect(part3(input)).toBe(0)
+})
---
+++ project_templates/ts/src/xService.ts
@@ -0,0 +1,11 @@
+export const part1 = (inputLines: string[]): number => {
+    return 0
+}
+
+export const part2 = (inputLines: string[]): number => {
+    return 0
+}
+
+export const part3 = (inputLines: string[]): number => {
+    return 0
+}
